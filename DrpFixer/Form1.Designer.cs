fa38d36e9f9fd70113bfeef0ebe1dbc30b1d08de
DrPanayioths <panayioths0072019@gmail.com>
2024-01-02 22:26:39 +0200

    Changes And Add Click Receiver

--- DrpFixer/Form1.Designer.cs
+++ DrpFixer/Form1.Designer.cs
@@ -5,13 +5,16 @@ namespace DrpFixer
     partial class drpdownloader
     {
         private System.Windows.Forms.Button Epicg;
+        private System.Windows.Forms.Button Steam;
+        private System.Windows.Forms.Button Origin;
+        private System.Windows.Forms.Button EA;
 
         private void InitializeComponent()
         {
             this.Epicg = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Steam = new System.Windows.Forms.Button();
+            this.Origin = new System.Windows.Forms.Button();
+            this.EA = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Epicg
@@ -25,43 +28,46 @@ namespace DrpFixer
             this.Epicg.TabIndex = 0;
             this.Epicg.Text = "Epic Games";
             this.Epicg.UseVisualStyleBackColor = true;
-            this.Epicg.Click += new System.EventHandler(this.Epicg_Click);
+            this.Epicg.Click += new System.EventHandler(this.Epic_Click);
             // 
-            // button1
+            // Steam
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(125, 387);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 41);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Steam";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Steam.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Steam.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Steam.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Steam.Location = new System.Drawing.Point(125, 387);
+            this.Steam.Name = "Steam";
+            this.Steam.Size = new System.Drawing.Size(107, 41);
+            this.Steam.TabIndex = 1;
+            this.Steam.Text = "Steam";
+            this.Steam.UseVisualStyleBackColor = true;
+            this.Steam.Click += new System.EventHandler(this.Steam_Clicker);
             // 
-            // button2
+            // Origin
             // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(238, 387);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 41);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Origin";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Origin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Origin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Origin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Origin.Location = new System.Drawing.Point(238, 387);
+            this.Origin.Name = "Origin";
+            this.Origin.Size = new System.Drawing.Size(107, 41);
+            this.Origin.TabIndex = 2;
+            this.Origin.Text = "Origin";
+            this.Origin.UseVisualStyleBackColor = true;
+            this.Origin.Click += new System.EventHandler(this.Origin_Clicker);
             // 
-            // button3
+            // EA
             // 
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(351, 387);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 41);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "EA";
-            this.button3.UseVisualStyleBackColor = true;
+            this.EA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EA.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.EA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EA.Location = new System.Drawing.Point(351, 387);
+            this.EA.Name = "EA";
+            this.EA.Size = new System.Drawing.Size(107, 41);
+            this.EA.TabIndex = 3;
+            this.EA.Text = "EA";
+            this.EA.UseVisualStyleBackColor = true;
+            this.EA.Click += new System.EventHandler(this.ea_Clicke);
             // 
             // drpdownloader
             // 
@@ -69,9 +75,9 @@ namespace DrpFixer
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EA);
+            this.Controls.Add(this.Origin);
+            this.Controls.Add(this.Steam);
             this.Controls.Add(this.Epicg);
             this.Name = "drpdownloader";
             this.ShowIcon = false;
@@ -82,13 +88,25 @@ namespace DrpFixer
 
         }
 
-        private void Epicg_Click(object sender, EventArgs e)
+        private void Epic_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Steam_Clicker(object sender, EventArgs e)
+        {
+
+        }
+        private void Origin_Clicker(object sender, EventArgs e)
+        {
+
+        }
+        private void ea_Clicke(object sender, EventArgs e)
         {
 
         }
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+
+
+
     }
 }
