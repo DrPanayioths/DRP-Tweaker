62080566a9124b18b2afa465648181417cf2140e
DrPanayioths <panayioths0072019@gmail.com>
2024-08-14 04:05:38 +0300

    Logo Added, File Downloaded In Downloads Folder
    My Personal Logo Added To The Top Left of the Window, Files that are downloaded are now saved in the downloads folder of your pc

--- Python Version/gui.py
+++ Python Version/gui.py
@@ -1,19 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import PhotoImage
 from threading import Thread
+from pathlib import Path
 import requests
+import os
+
 
 start = tk.Tk()
 start.geometry('1200x700')
 start.title('')
 
 
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+icon = PhotoImage(file=script_dir + "\drp.png")
+start.iconphoto(False, icon) 
+
 # Cloudflare 1.1.1.1
 def cloud():
     url = 'https://1111-releases.cloudflareclient.com/win/latest'
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
     filename = 'Cloudflare.msi'
-    with open(filename, 'wb') as f:
+    downloads_path = Path.home() / "Downloads"
+    file_path = downloads_path / filename
+    
+    with open(file_path, 'wb') as f:
         for chunk in response.iter_content(chunk_size=8192):
          if chunk:
               f.write(chunk)
